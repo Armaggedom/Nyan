db2292a1800c43caa777df18a2c9b545bb4ce386
Bombbom <doctor12221@gmail.com>
2021-11-26 17:12:43 -0300

    V:3.1.0 Corrections

--- security.js
+++ security.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 /* security.js
  * V: 1.0
 */
-//not work
 async function Security(command, message) {
 	var myString=command
 	var splits = myString.split('/');
@@ -22,29 +20,4 @@ function EmbedReturns(message) {
 		.setColor("#FF0000");
 	return message.channel.send(Embed)
 }
-=======
-/* security.js
- * V: 1.0
-*/
-//not work
-async function Security(command, message) {
-	var myString=command
-	var splits = myString.split('/');
-	for(var i=0; i<10; i++) {
-		if(splits[i]==='discord.gg'){
-			await message.delete({timeout:1})
-			return EmbedReturns(message)
-		}
-	}
-}
-function EmbedReturns(message) {
-	const Embed=new Discord.MessageEmbed()
-		.setTitle(`Hm`)
-		.setDescription(`\`\`\`Hm... \nVocê sabe que isso não é permitido!\`\`\``)
-		.setFooter(message.author.username+' => Código de regra: 1.1 - ID de penalidade 1')
-		.setTimestamp()
-		.setColor("#FF0000");
-	return message.channel.send(Embed)
-}
->>>>>>> 49c56408cff075556cdfc560cc4536faca9ef22e
 module.exports = Security;
